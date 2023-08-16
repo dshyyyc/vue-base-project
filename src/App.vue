c8abf188a1636cd67dffe797d93e02f45897cacd
dshyyyc <1162565246@qq.com>
2023-08-16 08:52:39 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+</template>
+
+<style lang="less">
+@import './common/style/mixin';
+</style>
+
